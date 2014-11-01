15f540d75cbd2d762d7c3b29ba390f56cbdbc97e
aperiodic <dlp@aperiodic.org>
2014-11-01 15:03:18 -0700

    Initial commit.
    Commit project.clj, lein-generated core.clj, LICENSE, and .gitignore.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-markov "0.1.0-SNAPSHOT"
+  :description "Markov chain training & generation."
+  :url "https://github.com/aperiodic/clj-markov"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
